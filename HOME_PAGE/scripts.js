d3504d9e308f3ab6b62e2a291fdcb09ff4f3b634
Karol Byczek <byczek_1986@interia.pl>
2023-12-13 20:15:06 +0100

    Creation of the homepage for the webstore application.

--- HOME_PAGE/scripts.js
+++ HOME_PAGE/scripts.js
@@ -1,3 +1,81 @@
-const $headline0 = document.getElementById("headline0");
+/* A non-css approach to exposition of submenus in the navigation list:
 
-$headline0.addEventListener("click", () => {console.log("STOP!!!")});
+const $menunav = document.querySelector(".home_navigation");
+const $submenus = document.querySelectorAll(".submenu");
+
+
+$menunav.addEventListener("mouseover", ({ target }) => {
+    if (target.classList.contains("menu_items")) {
+      target.children[0].style.visibility = "visible";
+      target.children[0].style.opacity = "1";
+    }
+  });
+
+  $menunav.addEventListener("mouseout", ({ target }) => {
+    if (target.classList.contains("menu_items")) {
+      target.children[0].style.visibility = "hidden";
+      target.children[0].style.opacity = "0";
+    }
+  });
+
+  $submenus.forEach(submenu => {submenu.addEventListener("mouseover", () => {
+      submenu.style.visibility = "visible";
+      submenu.style.opacity = "1";
+    }
+  )});
+
+  $submenus.forEach(submenu => {submenu.addEventListener("mouseout", () => {
+    submenu.style.visibility = "hidden";
+    submenu.style.opacity = "0";
+  }
+)});
+*/
+
+const $logButton = document.querySelector("#login_button");
+const $logStrip = document.querySelector("#login_strip");
+
+$logButton.addEventListener("click", () => {
+  $logStrip.classList.toggle("active");
+});
+
+const $register = document.querySelector("#register");
+const $backDrop = document.querySelector(".backdrop");
+
+
+$register.addEventListener("click", () => {
+  event.preventDefault();
+  $backDrop.classList.add("active");
+});
+
+
+const bestsellerCollection = [
+  {img1: "dolcegabana_stock_001a.jpg", img2: "dolcegabana_stock_001b.jpg", dcr: "Description of a product..."},
+  {img1: "jeanpaulgotier_stock_001a.jpg", img2: "jeanpaulgotier_stock_001b.jpg", dcr: "Another description of a product..."},
+  {img1: "matsuda_stock_001a.webp", img2: "matsuda_stock_001b.webp", dcr: "Yet another description of a product..."}
+];
+
+const $bestsellers = document.getElementById("bestseller_container");
+
+bestsellerCollection.forEach(bestseller => {
+  const mainblock = document.createElement("div");
+  mainblock.setAttribute("id", "mainblock");
+  const image1 = document.createElement("img");
+  image1.src = `images/${bestseller.img1}`;
+  image1.setAttribute("id", "image1");
+  const image2 = document.createElement("img");
+  image2.src = `images/${bestseller.img2}`;
+  image2.setAttribute("id", "image2");
+  const description = document.createElement("p");
+  description.innerHTML = bestseller.dcr;
+  description.setAttribute("id", "description");
+
+  const pictures = document.createElement('div');
+  pictures.setAttribute('id', 'pictures');
+
+  mainblock.appendChild(pictures);
+  mainblock.appendChild(description);
+  pictures.appendChild(image1);
+  pictures.appendChild(image2);
+
+  $bestsellers.appendChild(mainblock);
+});
