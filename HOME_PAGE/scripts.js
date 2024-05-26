c25b59c4f72fdbb468903929a03b41a399927161
KarolByczek <byczek_1986@interia.pl>
2024-05-26 15:37:51 +0200

    Adjustments in style and responsiveness.

--- HOME_PAGE/scripts.js
+++ HOME_PAGE/scripts.js
@@ -64,38 +64,38 @@ $menuItems.forEach((menuitem) => {
 const bestsellerCollection = [
   {
     pname: "Buena Vista Suit 001",
-    img1: "buenavista_stock_001a.jpg",
-    img2: "buenavista_stock_001b.jpg",
+    img1: "bella-gina-001a.jpg",
+    img2: "bella-gina-001d.jpg",
     dcr: "Out of the ashes of routine and blandness comes the greatly refreshing but still simple design. A perfect addition to your wardrobe dedicated for smart business outfits."
   },
   {
     pname: "Dark Hills Hat 001",
-    img1: "darkhills_stock_001a.webp",
-    img2: "darkhills_stock_001b.webp",
+    img1: "12ffdabefdbc892d3739ea60ff64d202.jpg",
+    img2: "d3a98b49679c032472d13f7a5bc39442.jpg",
     dcr: "This classic and outragously feminine hat, carries plenty of eye-catching features with its frivolity and outline finesse. Coming in three distinct variants in regards to color."
   },
   {
     pname: "Lavuasier Dress 002",
-    img1: "lavuasier_stock_002a.jpg",
-    img2: "lavuasier_stock_002b.jpg",
+    img1: "girl-pants-blous-isolated-white-background-body-language-63107751.jpg",
+    img2: "girl-pants-blous-partial-barrier-formed-isolated-white-background-body-language-63049021.jpg",
     dcr: "Orientally stylish, this kimono dress is the most beloved design in the Lavuasier's newest array of evening attire. Brightly colored, yet moderately unassuming, it makes an excellent choice for a fancy party."
   },
   {
     pname: "Primero Dress 001",
-    img1: "primero_stock_001a.jpg",
-    img2: "primero_stock_001b.jpg",
+    img1: "istockphoto-836262254-1024x1024.jpg",
+    img2: "istockphoto-836287926-1024x1024.jpg",
     dcr: "May we present to you the latest pride of the Primero brand. In its simplicity you will also find abundance of style, grace and sexiness. It will definitely satisfy tastes of modest ladies as well as those with flirtatious tricks in mind."
   },
   {
     pname: "Macinta Suit 001",
-    img1: "macinta_stock_001a.jpg",
-    img2: "macinta_stock_001b.jpg",
+    img1: "strict-young-woman-blouse-skirt-20981461.jpg",
+    img2: "strict-young-woman-blouse-skirt-21391050.jpg",
     dcr: "Tired of plain darkness ruling your business costume department? This modern otulook on the subject will keep your appearance at work uot of the boundaries of triviality while remaining within the zone of respectfulness and appropriate status."
   },
   {
     pname: "Macinta Dress 002",
-    img1: "macinta_stock_002a.jpg",
-    img2: "macinta_stock_002b.jpg",
+    img1: "young-emotional-woman-pink-dress-11264435.jpg",
+    img2: "young-emotional-woman-pink-dress-11264441.jpg",
     dcr: "Lorem ipsum. Lorem ipsum lorem ipsum lorem ipsum lorem ipsum. Lorem ipsum Lorem ipsum. Lorem ipsum lorem ipsum lorem ipsum lorem ipsum. Lorem ipsum. Lorem ipsum lorem ipsum lorem ipsum lorem ipsum Niniejsza strona korzysta z plików cookie. Wykorzystujemy pliki cookie do spersonalizowania treści i reklam, aby oferować funkcje społecznościowe i analizować ruch w naszej witrynie. Informacje o tym, jak korzystasz z naszej witryny, udostępniamy partnerom społecznościowym, reklamowym i analitycznym. Partnerzy mogą połączyć te informacje z innymi danymi otrzymanymi od Ciebie lub uzyskanymi podczas korzystania z ich usług.",
   }
 ];
